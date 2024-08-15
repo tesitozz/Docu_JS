74dd87c8973fb7bb55ddb4db91d721ed5b766cfe
tesitozz <ds1770767@gmail.com>
2024-08-14 19:39:45 -0500

    Se agrega Iteradores

---
+++ 11 - Iteradores/JS/01 - app.js
@@ -0,0 +1,54 @@
+
+// For Loop
+
+// for (let i = 0; i <= 10; i++) {
+
+//     console.log(` Numeros : ${i}`);
+
+
+// };
+
+//Identificar pares y impares
+
+// for (let i = 1; i <= 20; i++) {
+
+//     if (i % 2 === 0) {
+//         console.log(`El numero ${i} es par`)
+//     } else {
+//         console.log(`El numero ${i} es impar`)
+
+//     }
+
+// }
+
+const carrito = [
+    {
+        nombre: 'Monitor',
+        precio: 200
+    },
+    {
+        nombre: 'Celular',
+        precio: 300
+    },
+    {
+        nombre: 'Tablet',
+        precio: 300
+    },
+    {
+        nombre: 'Audifonos',
+        precio: 600
+    },
+    {
+        nombre: 'Mouse',
+        precio: 100
+    }
+]
+
+console.log(carrito.length);
+
+
+for (let i = 0; i < carrito.length; i++) {
+
+    console.log(carrito[i].nombre);
+
+}
